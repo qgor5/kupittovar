66ebe04aa67b9f49b46f1e4976cf2e272985616e
Віктор <agor832@gmail.com>
2025-06-18 11:40:59 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: 1,
+    name: "Еко-рюкзак",
+    price: 499,
+    description: "Стильний та зручний еко-рюкзак з міцного матеріалу.",
+    category: "Рюкзаки",
+    image: "https://cdn.pixabay.com/photo/2017/06/11/01/03/backpack-2393238_1280.jpg"
+  },
+  {
+    id: 2,
+    name: "Кавоварка автоматична",
+    price: 2999,
+    description: "Сучасна кавоварка з сенсорним управлінням і швидким приготуванням.",
+    category: "Кухонна техніка",
+    image: "https://cdn.pixabay.com/photo/2017/02/10/12/48/coffee-2055074_1280.jpg"
+  },
+  {
+    id: 3,
+    name: "Спортивні кросівки",
+    price: 1299,
+    description: "Легкі та комфортні кросівки для бігу та тренувань.",
+    category: "Взуття",
+    image: "https://cdn.pixabay.com/photo/2016/11/29/09/17/shoes-1866576_1280.jpg"
+  }
+];
