0b25c001b267aa4c2af7e9ffa5235504ae3355ff
Віктор <agor832@gmail.com>
2025-06-18 13:50:27 +0300

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,1 +1,23 @@
-// Массив товаров, загружаемых вручную
+const products = [
+  {
+    id: 1,
+    name: "Смарт-годинник A1",
+    price: 799,
+    image: "img/watch.jpg",
+    description: "Сучасний смарт-годинник із підтримкою Bluetooth."
+  },
+  {
+    id: 2,
+    name: "Навушники X200",
+    price: 399,
+    image: "img/headphones.jpg",
+    description: "Безпровідні навушники із зарядним кейсом."
+  },
+  {
+    id: 3,
+    name: "Портативна колонка Bass+",
+    price: 599,
+    image: "img/speaker.jpg",
+    description: "Гучна колонка з потужним басом та Bluetooth-з'єднанням."
+  }
+];
